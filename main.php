d40e4123cb4027db97222af89eb30476ebaa259c
dokland <66390699+dokland@users.noreply.github.com>
2020-07-18 21:15:38 +0100

    Create main.php

---
+++ main.php
@@ -0,0 +1,23 @@
+<h5>Domain Resolver</h5>
+<div class="content">
+<form action="" method="post">
+	<br><br>
+       <input class="textbox" type="text" name="domain">
+       <button type="submit" type="submit"><b>GET</b></button>
+</form>
+
+<?php
+if (isset($_POST['submit'])){
+     $domain = $_POST['domain'];
+     echo " $domain_ip";
+  }
+ ?>
+
+ <?php
+ $domain = $_POST['domain'];
+ $ip = gethostbyname($domain);
+
+    echo "Domain: ".$domain;
+    echo "<br>";
+    echo "IP: ".$ip;
+ ?>
